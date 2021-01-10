6d5052f6348f5f10a05ebcf3e84ae263fe0e6b66
Aleksei <lex6164665@yandex.ru>
2021-01-10 16:54:09 +0300

    update script file! added personalMovieDB, question

--- js/script.js
+++ js/script.js
@@ -1,1 +1,26 @@
 "use strict";
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", ""),
+questionNameFilm =  "Один из последних просмотренных фильмов?",
+questionRatingFilm = "На сколько оцените его?";
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+};
+
+const answer1_1 = prompt(questionNameFilm, ""),
+    answer1_2 = prompt(questionRatingFilm, ""),
+    answer2_1 = prompt(questionNameFilm, ""),
+    answer2_2 = prompt(questionRatingFilm, "");
+
+personalMovieDB.movies[answer1_1] = answer1_2,
+personalMovieDB.movies[answer2_1] = answer2_2,    
+
+console.log(personalMovieDB);
+
+
+
+
