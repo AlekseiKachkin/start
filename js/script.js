597be5d899c3f7ee36fdd95f333b99fe5b3f952d
Aleksei <lex6164665@yandex.ru>
2021-01-17 14:17:14 +0300

    modifed script.js file. added 2 cicle

--- js/script.js
+++ js/script.js
@@ -1,23 +1,69 @@
 "use strict";
-const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", ""),
+
+let questionNumberOfFilms = "Сколько фильмов вы уже посмотрели?",
 questionNameFilm =  "Один из последних просмотренных фильмов?",
-questionRatingFilm = "На сколько оцените его?";
+questionRatingFilm = "На сколько оцените его?",
+numberOfFilms, answerNameFilms, answerRatingFilms;
+
 
 const personalMovieDB = {
-    count: numberOfFilms,
+    count: '',
     movies: {},
     actors: {},
     genres: [],
     privat: false,
 };
 
-const answer1_1 = prompt(questionNameFilm, ""),
-    answer1_2 = prompt(questionRatingFilm, ""),
-    answer2_1 = prompt(questionNameFilm, ""),
-    answer2_2 = prompt(questionRatingFilm, "");
 
-personalMovieDB.movies[answer1_1] = answer1_2,
-personalMovieDB.movies[answer2_1] = answer2_2,    
+while(!numberOfFilms) {
+    numberOfFilms = +prompt(questionNumberOfFilms, "");
+    if (isNaN(numberOfFilms)) {
+        alert ("введено неверное значение!");
+        numberOfFilms = undefined;
+    }
+    personalMovieDB.count = numberOfFilms;
+
+}
+
+// for(let i = 1; i <= 2; ++i){
+//     answerNameFilms = prompt(questionNameFilm, "");
+//     if (!answerNameFilms || answerNameFilms.length >  50){
+//         alert ('Ошибка ввода!');
+//         answerNameFilms = undefined;
+//         --i; 
+//         continue;
+//     }   
+//     answerRatingFilms = +prompt(questionRatingFilm);
+//     if (!answerRatingFilms || isNaN(answerRatingFilms)){
+//         alert ('Ошибка ввода!');
+//         answerNameFilms = undefined;
+//         --i; 
+//         continue;
+//     }
+//     personalMovieDB.movies[answerNameFilms] = answerRatingFilms;
+// }
+let i = 0;
+while (i<2){   
+    answerNameFilms = prompt(questionNameFilm, "");
+    answerRatingFilms = +prompt(questionRatingFilm);
+    if (answerNameFilms != null && answerNameFilms.length < 50 && answerRatingFilms != null && !isNaN(answerRatingFilms)){
+        personalMovieDB.movies[answerNameFilms] = answerRatingFilms;
+        ++i;
+        continue;
+    }
+    alert('ошибка ввода!'); 
+}
+
+if ( (personalMovieDB.count < 10) && (personalMovieDB.count > 0) ) {
+    alert('Просмотрено довольно мало фильмов');
+}else if (personalMovieDB.count < 30 ) {
+    alert('Вы классический зритель');    
+}else if(personalMovieDB.count >= 30) {
+    alert('Да вы киноман!');
+} else {
+    alert('Произошла ошибка!');
+}
+    
 
 console.log(personalMovieDB);
 
